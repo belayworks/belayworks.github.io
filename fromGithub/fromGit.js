0fb872da0ee17a9b9ce165754343a05f1286190c
belayworks <147942076+belayworks@users.noreply.github.com>
2023-10-20 20:37:44 -0500

    Add files via upload

--- fromGithub/fromGit.js
+++ fromGithub/fromGit.js
@@ -55,7 +55,7 @@ checkLabel1.textContent = "The Country";
 let checkInput2 = document.createElement("input");
 checkInput2.type = "checkbox";
 let checkLabel2 = document.createElement("label");
-checkLabel2.textContent = "The Country";
+checkLabel2.textContent = "The Globe";
 
 ///////////
 let textAr = document.createElement("textarea");
@@ -94,8 +94,8 @@ imgLabel.innerHTML = "flower";
 // }
 
 document.body.append(
-  radLabel1,
   radInput1,
+  radLabel1,
   brLine1,
   radInput2,
   radLabel2,
@@ -170,25 +170,191 @@ function changeDisplay() {
   }
 }
 
-
 let visDiv = document.querySelector("#visDIV");
-visDiv.style.backgroundColor= "lightblue"
-let visInput = document.querySelector("input")
-visInput.style.visibility = "visible"
-visInput.style.width="200px"
+visDiv.style.backgroundColor = "lightblue";
+let visInput = document.querySelector("input");
+visInput.style.visibility = "visible";
+visInput.style.width = "200px";
 let visLab = document.createElement("label");
-visLab.innerHTML= "Visibility";
+visLab.innerHTML = "Visibility";
 
 visDiv.append(visLab, visInput);
-document.body.append(visDiv)
+document.body.append(visDiv);
 
-
-function changeVisib(){
-
+function changeVisib() {
   currentVisib = visInput.style.visibility;
-  if(currentVisib==="visible"){
-    visInput.style.visibility="hidden"
-  }else if(currentVisib==="hidden"){
-    visInput.style.visibility="visible"
+  if (currentVisib === "visible") {
+    visInput.style.visibility = "hidden";
+  } else if (currentVisib === "hidden") {
+    visInput.style.visibility = "visible";
   }
-};
+}
+
+/////display part of a page conditionally
+//
+let pageSel = document.createElement("select");
+// let but1 = document.getElementById("li1But");
+// let but2 = document.getElementById("li2But");
+let dispLabel = document.createElement("label");
+dispLabel.innerHTML = "Scriptures";
+
+let opPage1 = document.createElement("option");
+opPage1.innerHTML = "Upanishads";
+let opPage2 = document.createElement("option");
+opPage2.innerHTML = "Vedas";
+
+let divLi1 = document.createElement("div");
+let ch1 = document.createElement("input");
+ch1.type = "checkbox";
+let lCh1 = document.createElement("label");
+lCh1.innerHTML = "Aiterya";
+let ch2 = document.createElement("input");
+ch2.type = "checkbox";
+let lCh2 = document.createElement("label");
+lCh2.innerHTML = "Brihadarankya";
+let ch3 = document.createElement("input");
+ch3.type = "checkbox";
+let lCh3 = document.createElement("label");
+lCh3.innerHTML = "AiteryaChandogya";
+let ch4 = document.createElement("input");
+ch4.type = "checkbox";
+let lCh4 = document.createElement("label");
+lCh4.innerHTML = "Mandukya";
+
+////
+let divLi2 = document.createElement("div");
+
+let chA = document.createElement("input");
+chA.type = "checkbox";
+let lChA = document.createElement("label");
+lChA.innerHTML = "Rig";
+let chB = document.createElement("input");
+chB.type = "checkbox";
+let lChB = document.createElement("label");
+lChB.innerHTML = "Yajur";
+let chC = document.createElement("input");
+chC.type = "checkbox";
+let lChC = document.createElement("label");
+lChC.innerHTML = "Adarvana";
+let chD = document.createElement("input");
+chD.type = "checkbox";
+let lChD = document.createElement("label");
+lChD.innerHTML = "Sama";
+let divAll = document.createElement("div");
+
+
+let showBut = document.getElementById("li1But");
+let subBut = document.getElementById("li2But");
+let hrLi = document.getElementById("hrpg");
+divLi2.append(chA, lChA, chB, lChB, chC, lChC, chD, lChD);
+divLi1.append(ch1, lCh1, ch2, lCh2, ch3, lCh3, ch4, lCh4);
+divAll.append(divLi1, divLi2);
+pageSel.append(opPage1, opPage2);
+document.body.append(dispLabel, pageSel, showBut, divAll, hrLi, subBut);
+
+function showOpion() {
+  let selectedOption = pageSel.value;
+  if (selectedOption === "Upanishads") {
+    divLi1.style.display = "block";
+    divLi2.style.display = "none";
+  } else if (selectedOption === "Vedas") {
+    divLi1.style.display = "none";
+    divLi2.style.display = "block";
+  }
+}
+
+function hideContent() {
+  let selectedOption = pageSel.value;
+  // pageSel.visibility = "visible";
+  if (selectedOption === "Upanishads" || selectedOption === "Vedas") {
+    divLi1.style.display = "none";
+    divLi2.style.display = "none";
+  }
+}
+
+// checked unchecked
+let thisDiv = document.createElement("div");
+let radInpA = document.createElement("input");
+radInpA.type = "radio";
+let rdLabA = document.createElement("label");
+rdLabA.innerHTML = " Hello";
+let radInpB = document.createElement("input");
+radInpB.type = "radio";
+let rdLabB = document.createElement("label");
+rdLabB.innerHTML = "hi";
+let chInp = document.createElement("input");
+chInp.type = "checkbox";
+let chLab = document.createElement("label");
+chLab.innerHTML = "checkbox";
+
+let inpBUt = document.getElementById("unckBut");
+
+
+thisDiv.append(radInpA, rdLabA, radInpB, rdLabB, chInp, chLab);
+document.body.append(thisDiv, inpBUt);
+
+function uncheckIt() {
+//   if (radInpA.checked || radInpB.checked || chInp.checked) {
+//     radInpA.checked = false;
+//     radInpB.checked = false;
+//     chInp.checked = false;
+//   }
+// }
+
+let allInputs=document.getElementsByTagName("input");
+for(let element of allInputs ){
+  element.checked=false;
+}
+}
+
+
+// create/sort/delete
+let delDIV1 = document.createElement("div");
+let delDIV2 = document.createElement("div");
+
+let imgOne = document.createElement("img");
+imgOne.src = "./flower2.jpeg";
+imgOne.style.width = "50px";
+let pOne = document.createElement("p");
+pOne.innerHTML = "Maharishi";
+let pTwo = document.createElement("p");
+pTwo.innerHTML = "Mahesh";
+
+let imgTwo = document.createElement("img");
+imgTwo.src = "./flower.jpg"
+imgTwo.style.width = "50px";
+let pThree = document.createElement("p");
+pThree.innerHTML = "John";
+let pFour = document.createElement("p");
+pFour.innerHTML = "Doe";
+
+let sortIt = document.getElementById("sortIt");
+let del1 = document.getElementById("deleteI");
+let del2 = document.getElementById("deleteII");
+let delDIVAll= document.createElement("div");
+delDIVAll.style.display = "flex";
+
+delDIV1.append(imgOne, pOne, pTwo, del1);
+delDIV2.append(imgTwo, pThree, pFour,del2)
+delDIVAll.append(sortIt,delDIV1, delDIV2)
+document.body.append(delDIVAll)
+
+function deleteOne(){
+  if(delDIV1.style.display!=="none"){
+    delDIV1.style.display="none"
+  }
+}
+function deleteTwo() {
+  if (delDIV2.style.display !== "none") {
+    delDIV2.style.display = "none";
+  }
+}
+function showAll() {
+  if (delDIV1.style.display === "none"|| delDIV2.style.display==="none") {
+   delDIV1.style.display = "block";
+   delDIV2.style.display = "block";
+  }
+}
+
+
+
